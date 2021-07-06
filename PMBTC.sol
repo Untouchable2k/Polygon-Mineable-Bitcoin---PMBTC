1a7f079724e9f6276962a989d9d6fb4405ed0e76
CrazyCaptian <dottage@gmail.com>
2021-07-06 00:10:24 -0500

    updatez

--- PMBTC.sol
+++ PMBTC.sol
@@ -861,10 +861,10 @@ function PNewHeap() public payable {
     emit SetHeap(address(0), address(heap));
 }
 
-//Cuts the difficulty in half for 
+//3x Easier difficulty in mining 
 function pThirdDifficulty() public payable {
     require(IERC20(PMBTCGuildContract).balanceOf(msg.sender) >= (100 * (reward_amount.divRound(50))), "Must have larger balance of HPz, first era is 100 matic spent in the guild contract");//  //costs 80 matic spent to reset ThirdDifficulty
-    require(msg.value >= 250*oneNineDigit * reward_amount.div(50), "Must send more Matic to lower difficulty by 2x, first era is 250 matic");  
+    require(msg.value >= 400*oneNineDigit * reward_amount.div(50), "Must send more Matic to lower difficulty by 3x, first era is 400 matic");  
             
 	    miningTarget = miningTarget.mult(3);
 	    
